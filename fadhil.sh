ecead72cc24ea337ea13696a8f5c70089c600308
fadhil7 <49116470+fadhil7@users.noreply.github.com>
2019-03-31 19:33:25 +0800

    Add files via upload

---
+++ fadhil.sh
@@ -0,0 +1,178 @@
+#!/system/xbin/bash
+clear
+git clone https://github.com/amsitlab/smsid-java.git
+cd ./smsid-java
+chmod +x ./install
+apt install ./smsid_1.1_all.deb
+clear
+
+toilet -f slant --gay "Fadhil-Spam"
+sleep 1
+echo "\033[33;1m Tools Spam BY: FADHIL-RINTIK"
+sleep 1
+echo "\033[36;1m★""\033[34;1mFADHIL""\033[36;1m★"
+sleep 1
+echo "\033[32;1m Spam Yang Tersedia :"
+echo "\033[35;1m1.""\033[36;1mBukalapak  (WA)"
+echo "\033[35;1m2.""\033[36;1mTelkomsel" (SMS)
+echo "\033[35;1m3.""\033[36;1mGrab".          (Telepon)
+echo "\033[35;1m4.""\033[36;1mTokopedia" (Telepon)
+echo "\033[35;1m5.""\033[36;1mCodaShop" (SMS)
+echo "\033[35;1m6.""\033[36;1mSurveyon".  (Email)
+echo "\033[35;1m7.""\033[36;1mCustom".    (SMS)
+echo "\033[31;1m0. Keluar"
+echo "\033[37;1m81.""\033[30;1mFADHIL-RINTIK TAMVANS"
+echo "\033[33;1m Pilih Angka:"
+read mrrm
+
+if [ $mrrm = 1 ] || [ $mrrm = 1 ]
+then
+clear
+toilet -f slant "Bukalapak"
+echo "\033[31;1mJenis Spam:""\033[37;1mChat WhatsApp"
+echo "\033[31;1mBatas:""\033[37;1mUnlimited"
+echo "\033[31;1mPemograman:""\033[37;1mPHP"
+echo "\033[31;1mAuthor:""\033[37;1mFADHIL-RINTIK"
+sleep 5
+clear
+cd $HOME/LITESPAM/Gg/
+php 1
+fi
+
+if
+[ $mrrm = 2 ] || [ $mrrm = 2 ]
+then
+clear
+echo "\033[31;1m"
+toilet "T-Sel"
+echo "\033[31;1mJenis Spam:""\033[37;1mSMS"
+echo "\033[31;1mBatas:""\033[37;1mUnlimited"
+echo "\033[31;1mPemograman:""\033[37;1mPHP"
+echo "\033[31;1mAuthor:""\033[37;1mFADHIL-RINTIK"
+sleep 5
+clear
+cd $HOME/LITESPAM/Gg/
+php 2.php
+fi
+
+if [ $mrrm = 3 ] || [ $mrrm = 3 ]
+then
+clear
+echo "\033[36;1m"
+figlet "Grab"
+echo "\033[31;1mJenis Spam:""\033[37;1mTelepon"
+echo "\033[31;1mBatas:""\033[37;1mUnlimited"
+echo "\033[31;1mPemograman:""\033[37;1mPHP"
+echo "\033[31;1mAuthor:""\033[37;1mFADHIL-RINTIK"
+sleep 5
+clear
+cd $HOME/LITESPAM/Gg/
+php 3.php
+fi
+
+
+if [ $mrrm = 4 ] || [ $mrrm = 4 ]
+then
+clear
+toilet -f mono9 -F gay "Toked"
+echo "\033[31;1mJenis Spam:""\033[37;1mTelepon"
+echo "\033[31;1mBatas:""\033[37;1m1 Kali"
+echo "\033[31;1mPemograman:""\033[37;1mPHP"
+echo "\033[31;1mAuthor:""\033[37;1mFADHIL-RINTIK"
+sleep 5
+clear
+cd $HOME/LITESPAM/Gg/
+php 4.php
+fi
+
+if [ $mrrm = 5 ] || [ $mrrm = 5 ]
+then
+clear
+toilet -f slant -F gay "CodaShop"
+echo "\033[31;1mJenis Spam:""\033[37;1mSMS"
+echo "\033[31;1mBatas:""\033[37;1mUnlimited"
+echo "\033[31;1mPemograman:""\033[37;1mPHP"
+echo "\033[31;1mAuthor:""\033[37;1mFADHIL-RINTIK"
+echo
+echo
+echo "\033[33;1mCOPY LISENSI INI:" "\033[36;1mJDJ5JDEwJC9YMWRvNC5jcmNOSUw2dGdUaUt2d08ucWEvWURWSFNCTXI3U21wdkdsR1FMcVBSTW1oNUZH"
+sleep 1
+echo "\033[32;1mtunggu 5 Detik"
+sleep 5
+clear
+cd $HOME/LITESPAM/Gg/
+php 5.php
+fi
+
+if
+[ $mrrm = 6 ] || [ $mrrm = 6 ]
+then
+clear
+toilet -f standard -F gay "Surveyon "
+echo "\033[31;1mJenis Spam:""\033[37;1mEmail"
+echo "\033[31;1mBatas:""\033[37;1mUnlimited"
+echo "\033[31;1mPemograman:""\033[37;1mphp"
+echo "\033[31;1mAuthor:""\033[37;1mFADHIL-RINTIK"
+sleep 5
+cd $HOME/LITESPAM/Gg/
+php 6.php
+fi
+
+if
+[ $mrrm = 7 ] || [ $mrrm = 7 ]
+then
+clear
+toilet -f slant --gay "SMSID"
+echo "\033[31;1mJenis Spam:""\033[37;1mSMS"
+echo "\033[31;1mBatas:""\033[37;1m5"
+echo "\033[31;1mPemograman:""\033[37;1mBerjalan Langsung Pada Fadhil7"
+echo "\033[31;1mAuthor:""\033[37;1mFADHIL-RINTIK"
+echo
+echo
+echo "\033[33;1mInformasi Lebih Lanjut Kunjungi:""\033[36;1mhttps://amsitlab.github.io/smsid-java/"
+sleep 6
+smsid boom -y
+fi
+
+if
+[ $mrrm = 81 ] || [ $mrrm = 81 ]
+then
+clear
+toilet -f slant --gay "FADHIL-SPAM"
+echo "\033[34;1mContact:""\033[37;1m https://fb.me/IrsyadMaulana81"
+
+
+sleep 1
+echo "\033[35;1mPemograman:""\033[36;1m Bash"
+
+echo "\033[33;1mKarya: FADHIL"
+sleep 1
+echo "\033[32;1mVersi: V1"
+sleep 1
+echo "\033[36;1mTeam: RINTIK CYBER TEAM"
+sleep 1
+echo "\033[31;1mSubscribe YouTube""\033[37;1m Fadhil Rahman"
+echo "\033[35;1m Spesial Thanks To: "
+echo "\033[36;1m"
+echo "FADHIL"
+echo "RINTIK TEAM"
+echo "INDONESIA"
+echo "RINTIK CYBER ARMY"
+echo "@Since 31-03-2019"
+sleep 1
+echo "RINTIK CYBER TEAM"
+echo "\033[30;1m tunggu 8 detik"
+sleep 8
+sh fadhil.sh
+fi
+
+
+
+if
+[ $mrrm = 0 ] || [ $mrrm = 0 ]
+then
+echo "\033[31;1m Keluar"
+sleep 1
+echo "\033[32;1m Sampai berjumpa lagi :)"
+sleep 1
+fi
